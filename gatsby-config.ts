cac142cefd33ef9950c125c5d39d964a058bef1d
TheOstrichEgg <ostricheggyj@gmail.com>
2024-11-15 00:04:30 +0900

    Initialize
    Add deploy workflows

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: "/theostrichegg.github.io/",
   siteMetadata: {
     title: `TheOstrichEgg`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://theostrichegg.github.io/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
