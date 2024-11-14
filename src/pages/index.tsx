ee92af313d1e92719777d17dd034d81183ddc4fc
Tazo R <5188821+TheOstrichEgg@users.noreply.github.com>
2024-11-15 00:06:25 +0900

    Initialize Gatsby page (#1)
    Add Gatsby files (created by gatsby new)
    Remove Gatsby sample pages
    Add deploy workflows

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function IndexPage() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-white">
+      <h1 className="text-2xl font-bold text-black">TheOstrichEgg</h1>
+    </div>
+  );
+};
